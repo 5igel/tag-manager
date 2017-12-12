08d17d5c51050ac2d3a009adc6edfaadec7478e1
Serhii Kocherev <st.ixidor@gmail.com>
2017-12-12 09:25:29 +0200

    fix lib writer

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const { Music } = require('meta-music');
 const { basename } = require('path');
 const { getFileList } = require('./file-list');
+const id3 = require('id3-writer');
 const result = [];
-
-var id3 = require('id3-writer');
-
 
 const rename = (folder) => {
     getFileList(folder)
@@ -15,16 +13,26 @@ const rename = (folder) => {
 
                 //  Define the tags for your file using the ID (e.g. APIC) or the alias (see at bottom)
                 const tags = {
-                  title: `Глава ${file[3]}${file[4]} - ч.${file[6]}${file[7]}`,
-                  artist: "Роберт Хайнлайн",
-                  album: "Звездный десант",
-                  genre: "Аудиокнига"
+                    title: `Глава ${file[3]}${file[4]} - ч.${file[6]}${file[7]}`,
+                    artist: "Роберт Хайнлайн",
+                    album: "Звездный десант",
+                    genre: "Аудиокнига"
                 }
-                console.log(tags);
 
-                const prom = Music.write(filepath, tags);
+                const writer = new id3.Writer();
+                const fileToWrite = new id3.File(filepath);
+                const meta = new id3.Meta(tags);
 
-                result.push(prom);
+                result.push(new Promise((resolve, reject) => {
+                    writer.setFile(fileToWrite).write(meta, function(err) {
+
+                        if (err) {
+                            reject(err)
+                        }
+
+                        resolve();
+                    });
+                }));
             });
 
             Promise.all(result)
